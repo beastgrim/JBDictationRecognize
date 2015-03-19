fccdb1429c9386b31e6e88dbff9c3cac65c75dd9
beastgrim <beastgrim@gmail.com>
2015-03-19 17:12:51 +0300

    init

---
+++ JBDictationRecognize.h
@@ -0,0 +1,13 @@
+//
+//  JBDictationRecognize.h
+//  Over Task
+//
+//  Created by Богомолов Евгений on 19.03.15.
+//  Copyright (c) 2015 Евгений Богомолов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JBDictationRecognize : NSObject
+
+@end
---
+++ JBDictationRecognize.m
@@ -0,0 +1,13 @@
+//
+//  JBDictationRecognize.m
+//  Over Task
+//
+//  Created by Богомолов Евгений on 19.03.15.
+//  Copyright (c) 2015 Евгений Богомолов. All rights reserved.
+//
+
+#import "JBDictationRecognize.h"
+
+@implementation JBDictationRecognize
+
+@end
